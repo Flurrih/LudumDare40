b7e699b64db62aaed7b776b0a5aa7a8038bd2e9f
Tomek <tom.gabryelewicz@gmail.com>
2017-12-02 16:26:33 +0100

    added police effect controller script

---
+++ LD40/Assets/PoliceEffectController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceEffectController : MonoBehaviour {
+
+    public GameObject BlueSprite;
+    public float SwapTime;
+    float swapTimer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
