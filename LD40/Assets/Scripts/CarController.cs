510d087793f013368e702dc0d083746c7cb2bf9a
DESKTOP-HBKHA3T\Tomek <ghost744@gmail.com>
2017-12-02 22:50:58 +0100

    Added car spawners, added car lifetime and death

--- LD40/Assets/Scripts/CarController.cs
+++ LD40/Assets/Scripts/CarController.cs
@@ -9,12 +9,18 @@ public class CarController : MonoBehaviour {
     public float CarSpeed = 3.0f;
     public bool StoppedOnTrafficLight = false;
     public bool StoppedOnOtherCar = false;
+
+    public float CarLifeTime = 25.0f;
     // Update is called once per frame
     void Update () {
         if (StoppedOnTrafficLight != true)
             this.gameObject.GetComponent<Rigidbody>().AddForce(gameObject.transform.rotation * new Vector3(CarSpeed, 0f, 0f));
         else
             this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+        CarLifeTime -= Time.deltaTime;
+        if (CarLifeTime <= 0)
+            GameObject.Destroy(this.gameObject);
 	}
 
     public void Brake()
