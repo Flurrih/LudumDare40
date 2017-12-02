ee56f9eb2ca620617fadd1dd3f74f8c69aec86c8
Bartosz Kluchciński <bkluchcinski@gmail.com>
2017-12-02 16:37:56 +0100

    added ingredient shop

---
+++ LD40/Assets/Scripts/EnterShopController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnterShopController : MonoBehaviour
+{
+    public Text enterText;
+    public GameObject shop;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            enterText.gameObject.SetActive(true);   
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            enterText.gameObject.SetActive(false);   
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E) && enterText.enabled)
+            {
+                shop.SetActive(true);
+            }
+        }
+    }
+
+
+}
