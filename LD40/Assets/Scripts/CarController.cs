1930f3a05b0750ecc398a450c0655d399372adbb
Tomek <tom.gabryelewicz@gmail.com>
2017-12-02 05:37:43 +0100

    Added simple car movement (AI), added player with 3rd camera, added roads

---
+++ LD40/Assets/Scripts/CarController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    public float CarSpeed = 3.0f;
+	
+	// Update is called once per frame
+	void Update () {
+        this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x + CarSpeed * Time.deltaTime, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+	}
+}
