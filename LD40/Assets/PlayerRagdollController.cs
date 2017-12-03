343ad534ab1a5ad1befde2d863dfbf1d0debb9ae
Tomek <tom.gabryelewicz@gmail.com>
2017-12-03 11:37:35 +0100

    added ragdoll controller

---
+++ LD40/Assets/PlayerRagdollController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRagdollController : MonoBehaviour {
+
+    public GameObject Ragdoll;
+    public GameObject Player;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Car")
+        {
+            Player.SetActive(false);
+            Ragdoll.SetActive(true);
+        }
+    }
+}
